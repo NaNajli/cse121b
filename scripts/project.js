5a8f59d683b13775ebd57f5a9c8d9d47d330f879
NaNajli <nadurodriguez2@gmail.com>
2023-10-23 17:51:26 -0300

    Project

---
+++ scripts/project.js
@@ -0,0 +1,71 @@
+const rooomsElement = document.querySelector("#rooms");
+let bethroomList = [];
+
+
+const displayBethrooms = (bethrooms) => {
+
+    bethrooms.forEach(bethroom=>{
+
+        
+           let article = document.createElement("article");
+           let room = document.createElement("h1");
+           let img = document.createElement("img");
+           let price = document.createElement("p");
+           let amenities = document.createElement("p");
+           let  facilities = document.createElement("p");   
+           img.setAttribute("src",bethroom.imageUrl);
+           img.setAttribute("alt",bethroom.room);
+           img.setAttribute("alt",bethroom.price);
+           article.appendChild(img);
+           article.appendChild(price);
+           article.appendChild(amenities);
+           article.appendChild(facilities);   
+           room.textContent = bethroom.room;
+           price.textContent = bethroom.price;
+           amenities.textContent = bethroom.amenities;
+           facilities.textContent = bethroom.facilities;
+           
+           
+           rooomsElement.appendChild(article);
+          
+    })};
+
+
+      const getBethRooms = async ()=>{
+    
+       const response = await fetch("https://run.mocky.io/v3/a7a34c96-e286-4e77-b830-a7e000cbf622");
+       bethroomList = await response.json(); 
+       displayBethrooms(bethroomList);
+      
+        };   
+  
+      const reset = () =>{
+      rooomsElement.innerHTML="";
+}; 
+
+        const sortByroom = (bethrooms) =>{
+            reset();
+        
+            switch(document.querySelector("#sortByroom").value){
+                case "one":      
+                displayBethrooms(bethrooms.filter((bethroom) => bethroom.guess.includes("one")));
+                break;
+                case "two":
+                displayBethrooms(bethrooms.filter(bethroom =>bethroom.guess.includes("two")));
+                break;
+                case "three":
+                displayBethrooms(bethrooms.filter(bethroom =>bethroom.guess.includes("three")));   
+                    break;
+                case "four":
+                    displayBethrooms(bethrooms.filter(bethroom =>bethroom.guess.includes("four")));   
+                        break;           
+            }
+        };
+         
+        getBethRooms();
+     
+            
+        /* Event Listener */
+        document.querySelector("#sortByroom").addEventListener("change",()=>{sortByroom(bethroomList)});
+       
+
--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -7,15 +7,25 @@ let fullName = "Nadia Rodriguez";
 let currentYear = "2023";
 let profilePicture = "images/nadiaphoto.png"
 
+/* Step 3 - Element Variables */
+
 const nameElement = document.getElementById("name");
 const foodElement = document.getElementById("food");
 const yearElement = document.querySelector("#year");
 const imageElement = document.querySelector("img");
 
+
+/* Step 4 - Adding Content */
+
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
 yearElement.textContent = currentYear;
 imageElement.setAttribute("src", profilePicture);
 imageElement.setAttribute("alt", "Photo Nadia");
+
+
+
+/* Step 5 - Array */
+
 
 const favoriteFoods = [];
 favoriteFoods.push("Salad","Pizza","Ice-cream","Rice","Salmon")
@@ -33,20 +43,18 @@ foodElement.innerHTML += `<br>${favoriteFoods}`;
 
 
 
-/* Step 3 - Element Variables */
-
-
-
-
-
-/* Step 4 - Adding Content */
 
 
 
 
 
 
-/* Step 5 - Array */
+
+
+
+
+
+
 
 
 
